849c7d3d9f8b5e88a021b0075fd50028187a8216
Madeleine Roberts <msroberts@users.noreply.github.com>
2018-12-31 22:25:27 -0500

    Improve save filters

--- src/renderer/helpers/remote.ts
+++ src/renderer/helpers/remote.ts
@@ -1,7 +1,22 @@
-import { remote } from 'electron'
+import { remote, SaveDialogOptions, FileFilter } from 'electron'
 
-export function showSaveDialog () {
+export const DEFAULT_FILTERS: FileFilter[] = [
+  {
+    name: 'JSON',
+    extensions: [
+      'json',
+    ],
+  },
+]
+
+export function showSaveDialog (options?: SaveDialogOptions) {
+  if (!options) {
+    options = {
+      filters: DEFAULT_FILTERS,
+    }
+  }
+
   return new Promise<string>((resolve) => {
-    remote.dialog.showSaveDialog({}, resolve)
+    remote.dialog.showSaveDialog(options!, resolve)
   })
 }
