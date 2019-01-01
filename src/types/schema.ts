c84ab785a5a8a1138b1254fd13022702053b88b8
Madeleine Roberts <msroberts@users.noreply.github.com>
2018-12-31 22:18:55 -0500

    Improve types

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,7 +1,14 @@
-export interface IRecipe {
+export interface ISchema<T> {
+  '@context': 'http://schema.org/',
+  '@type': T,
+}
+
+export interface IRecipe extends ISchema<'Recipe'> {
   name: string,
 }
 
 export const RECIPE_DEFAULT: IRecipe = {
+  '@context': 'http://schema.org/',
+  '@type': 'Recipe',
   name: '',
 }
